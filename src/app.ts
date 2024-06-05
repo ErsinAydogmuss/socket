ca0ba61e1787920a6974bbe85f13c63ca6f402bc
ersinaydogmusss <ersinaydogmus@goaction.tech>
2024-06-05 06:11:44 +0300

    asdas

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,13 @@ const io = new SocketIOServer(server, {
 });
 
 const users = new Map<string, string>();
+interface Room {
+  user1: string;
+  user2: string;
+  connectionId: string;
+}
+
+const rooms: Map<string, Room> = new Map();
 
 io.on("connection", (socket) => {
   console.log("Bir kullanıcı bağlandı");
@@ -150,7 +157,7 @@ io.on("connection", (socket) => {
         senderId,
         receiverId,
         type,
-        unSeen: false,
+        unSeen: (notification.documents[0] as any)?.unSeen,
         postId,
         commentId,
         active,
@@ -181,6 +188,51 @@ io.on("connection", (socket) => {
       }
     }
   });
+
+  socket.on("joinRoom", async (keys) => {
+    console.log("joinRoom", keys);
+    const { connectionId, userID } = keys;
+
+    let room: Room | undefined;
+    // Verilen connectionId'ye ait bir oda var mı kontrol edelim
+    const speacialRoom = rooms.get(connectionId);
+    if (!speacialRoom) {
+      // Oda yoksa, yeni bir oda oluştur
+      rooms.set(connectionId, { user1: userID, user2: "", connectionId });
+
+      console.log("oda oluştu");
+    } else if (speacialRoom.user2 === "") {
+      // Oda varsa, ikinci kullanıcıyı ekleyin
+      speacialRoom.user2 = userID;
+      rooms.set(connectionId, speacialRoom);
+      console.log("ikinci user geldi");
+    } else if (speacialRoom.user1 === "") {
+      // Oda varsa, ikinci kullanıcıyı ekleyin
+      speacialRoom.user1 = userID;
+      rooms.set(connectionId, speacialRoom);
+      console.log("birinci user geldi");
+    }
+  });
+
+  socket.on("removeRoom", async (keys) => {
+    const { connectionId, userID } = keys;
+    const room = rooms.get(connectionId);
+    if (room) {
+      if (userID === room.user1) {
+        room.user1 = "";
+        console.log("user1 çıktı");
+      } else if (userID === room.user2) {
+        room.user2 = "";
+        console.log("user2 çıktı");
+      }
+      // Eğer her iki kullanıcı da odadan ayrıldıysa, odayı sil
+      if (room.user1 === "" && room.user2 === "") {
+        rooms.delete(connectionId);
+        console.log("room silindi");
+      }
+    }
+  });
+
   // send message
   socket.on("sendMessage", async (message) => {
     const {
@@ -195,94 +247,181 @@ io.on("connection", (socket) => {
       profileImageUrl,
     } = message;
 
-    if (control === false) {
-      const getConversation = await database.listDocuments(
-        "66397753002754b32828",
-        "6658b0d90035989e7b16",
-        [Query.equal("participants", conversationId)]
-      );
+    const room = rooms.get(conversationId);
 
-      await database.updateDocument(
-        "66397753002754b32828",
-        "6658b0d90035989e7b16",
-        getConversation.documents[0].$id,
-        {
-          lastMessage: text,
-          lastMessageId: senderId,
-          unSeen: false,
-        }
-      );
-    }
+    // check receiver on rooms
 
-    const receiverSocketId = users.get(receiverId);
-    if (receiverSocketId) {
-      console.log(text);
-      socket.to(receiverSocketId).emit("receiveMessage", {
-        senderId,
-        conversationId,
-        receiverId,
-        text,
-        unSeen,
-        active,
-        profileImageUrl,
-        senderName,
-      });
-      socket.emit("receiveMessage", {
-        senderId,
-        conversationId,
-        receiverId,
-        text,
-        unSeen: false,
-        active,
-        profileImageUrl,
-        senderName,
-      });
-      await database.createDocument(
-        "66397753002754b32828",
-        "6639776c003a4977f834",
-        ID.unique(),
-        {
+    if (receiverId === room?.user1 || receiverId === room?.user2) {
+      console.log("receiverId", receiverId);
+      console.log("room?.user1", room?.user1);
+      console.log("room?.user2", room?.user2);
+      if (control === false) {
+        const getConversation = await database.listDocuments(
+          "66397753002754b32828",
+          "6658b0d90035989e7b16",
+          [Query.equal("participants", conversationId)]
+        );
+
+        const updateConversation = await database.updateDocument(
+          "66397753002754b32828",
+          "6658b0d90035989e7b16",
+          getConversation.documents[0].$id,
+          {
+            lastMessage: text,
+            lastMessageId: senderId,
+            unSeen: true,
+          }
+        );
+      }
+
+      const receiverSocketId = users.get(receiverId);
+      if (receiverSocketId) {
+        console.log(text);
+        socket.to(receiverSocketId).emit("receiveMessage", {
           senderId,
           conversationId,
           receiverId,
           text,
-          unSeen: false,
+          unSeen: true,
           active,
-        }
-      );
+        });
+        socket.emit("receiveMessage", {
+          senderId,
+          conversationId,
+          receiverId,
+          text,
+          unSeen: true,
+          active,
+        });
+        await database.createDocument(
+          "66397753002754b32828",
+          "6639776c003a4977f834",
+          ID.unique(),
+          {
+            senderId,
+            conversationId,
+            receiverId,
+            text,
+            unSeen: true,
+            active,
+          }
+        );
+      } else {
+        socket.emit("receiveMessage", {
+          senderId,
+          conversationId,
+          receiverId,
+          text,
+          unSeen: true,
+          active,
+        });
+        await database.createDocument(
+          "66397753002754b32828",
+          "6639776c003a4977f834",
+          ID.unique(),
+          {
+            senderId,
+            conversationId,
+            receiverId,
+            text,
+            unSeen: true,
+            active,
+          }
+        );
+      }
     } else {
-      socket.emit("receiveMessage", {
-        senderId,
-        conversationId,
-        receiverId,
-        text,
-        unSeen: false,
-        profileImageUrl,
-        senderName,
-        active,
-      });
-      await database.createDocument(
-        "66397753002754b32828",
-        "6639776c003a4977f834",
-        ID.unique(),
-        {
+      console.log("receiverId", receiverId);
+      console.log("room?.user1", room?.user1);
+      console.log("room?.user2", room?.user2);
+      if (control === false) {
+        const getConversation = await database.listDocuments(
+          "66397753002754b32828",
+          "6658b0d90035989e7b16",
+          [Query.equal("participants", conversationId)]
+        );
+
+        await database.updateDocument(
+          "66397753002754b32828",
+          "6658b0d90035989e7b16",
+          getConversation.documents[0].$id,
+          {
+            lastMessage: text,
+            lastMessageId: senderId,
+            unSeen: false,
+          }
+        );
+      }
+
+      const receiverSocketId = users.get(receiverId);
+      if (receiverSocketId) {
+        console.log(text);
+        socket.to(receiverSocketId).emit("receiveMessage", {
           senderId,
           conversationId,
           receiverId,
           text,
           unSeen: false,
           active,
-        }
-      );
+          profileImageUrl,
+          senderName,
+        });
+        socket.emit("receiveMessage", {
+          senderId,
+          conversationId,
+          receiverId,
+          text,
+          unSeen: false,
+          active,
+          profileImageUrl,
+          senderName,
+        });
+        await database.createDocument(
+          "66397753002754b32828",
+          "6639776c003a4977f834",
+          ID.unique(),
+          {
+            senderId,
+            conversationId,
+            receiverId,
+            text,
+            unSeen: false,
+            active,
+          }
+        );
+      } else {
+        socket.emit("receiveMessage", {
+          senderId,
+          conversationId,
+          receiverId,
+          text,
+          unSeen: false,
+          profileImageUrl,
+          senderName,
+          active,
+        });
+        await database.createDocument(
+          "66397753002754b32828",
+          "6639776c003a4977f834",
+          ID.unique(),
+          {
+            senderId,
+            conversationId,
+            receiverId,
+            text,
+            unSeen: false,
+            active,
+          }
+        );
+      }
     }
+  });
 
-    socket.on("disconnect", () => {
-      console.log("Bir kullanıcı ayrıldı");
-      const deletedUserId = Array.from(users).find(
-        ([key, value]) => value === socket.id
-      )?.[0];
-      users.delete(deletedUserId!);
-    });
+  socket.on("disconnect", () => {
+    console.log("Bir kullanıcı ayrıldı");
+    const deletedUserId = Array.from(users).find(
+      ([key, value]) => value === socket.id
+    )?.[0];
+    users.delete(deletedUserId!);
   });
 });
 
